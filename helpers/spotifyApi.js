d0960a25a8af6901dde460bb53310c405b8e4e4d
Ihza Novellino <ihzan0@gmail.com>
2022-08-20 08:13:33 +0700

    Update spotifyApi.js

--- helpers/spotifyApi.js
+++ helpers/spotifyApi.js
@@ -1,7 +1,7 @@
 const SpotifyWebApi = require('spotify-web-api-node');
 let spotifyApi = new SpotifyWebApi({
-    clientId: '54ad5ca56d494d1ba439c7a366e3f09a',
-    clientSecret: 'f916b7c948fe4a48a71b39d2853c8562',
+    clientId: '',
+    clientSecret: '',
 });
 spotifyApi.clientCredentialsGrant()
     .then(data => {
